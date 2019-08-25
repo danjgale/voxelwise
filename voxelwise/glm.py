6e435a34b626cb384ad91ee913c955d64ae6dcfb
Dan Gale <danjgale@gmail.com>
2019-08-25 14:31:37 -0400

    added basic code outline

---
+++ voxelwise/glm.py
@@ -0,0 +1,24 @@
+"""Classes for voxel pattern extraction via GLM approaches"""
+
+
+class BaseGLM(object):
+    def __init__(self):
+        pass
+
+
+    def fit(self):
+        pass
+
+
+    def fit_transform(self):
+        pass
+
+
+class LSS(BaseGLM):
+    def __init__(self):
+        super().__init__(self)
+
+
+class LSA(BaseGLM):
+    def __init__(self):
+        super().__init__(self)
